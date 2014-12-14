3da14285a64a267658386029f36c883fb5bb355a
Sid <sidharth@knewton.com>
2014-12-14 18:30:25 -0500

    Will add examples back in a bit

--- example/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# TODO: Change the relative_testkit_path to '../rsTAP/test_kit/' when using it in your
-# project.
-psql -Xf test/ddl/test_example_ddl.sql -v project_root=`pwd` -v relative_testkit_path='../test_kit/'
